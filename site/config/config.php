5aa833afe97d03fcc470dba01051a01d70395b78
zylai <40312846+zylai@users.noreply.github.com>
2022-05-08 17:03:38 -0700

    Add files via upload

--- site/config/config.php
+++ site/config/config.php
@@ -1,11 +1,33 @@
 <?php
 return [
-    'debug' => true,
-    'panel' => false,
-    'thumbs' => [
-        'srcsets' => [
-            'default' => [800, 1200]
-        ]
+  'debug' => false,
+  'panel' => true,
+  'thumbs' => [
+    'srcsets' => [
+      'default' => []
     ]
-];
+  ],
+  'routes' => [
+    [
+      'pattern' => 'sitemap.xml',
+      'action'  => function() {
+        $pages = site()->pages()->index();
 
+          // fetch the pages to ignore from the config settings,
+          // if nothing is set, we ignore the error page
+        $ignore = kirby()->option('sitemap.ignore', ['error']);
+
+        $content = snippet('sitemap', compact('pages', 'ignore'), true);
+
+          // return response with correct header type
+        return new Kirby\Cms\Response($content, 'application/xml');
+      }
+    ],
+    [
+      'pattern' => 'sitemap',
+      'action'  => function() {
+        return go('sitemap.xml', 301);
+      }
+    ]
+  ]
+];
